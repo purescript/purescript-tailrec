e0f0acce3ad316bf6edd749d37eda414329b33b7
Phil Freeman <paf31@cantab.net>
2015-02-15 12:26:34 -0800

    Better docs

--- docs/README.md
+++ docs/README.md
@@ -6,6 +6,27 @@
 
 #### `MonadRec`
 
+This type class captures those monads which support tail recursion in constant stack space.
+
+The `tailRecM` function takes a step function, and applies that step function recursively
+until a return value of type `b` is found.
+
+Instances are provided for standard monad transformers.
+
+For example:
+
+```purescript
+loopWriter :: Number -> WriterT Sum (Eff (trace :: Trace)) Unit
+loopWriter n = tailRecM go n
+  where
+  go 0 = do
+    lift $ trace "Done!"
+    return (Right unit)
+  go n = do
+    tell $ Sum n  
+    return (Left (n - 1))
+```
+
     class (Monad m) <= MonadRec m where
       tailRecM :: forall a b. (a -> m (Either a b)) -> a -> m b
 
@@ -41,13 +62,30 @@
 
 #### `tailRec`
 
+Create a pure tail-recursive function of one argument
+
+For example:
+
+```purescript
+pow :: Number -> Number -> Number
+pow n p = tailRec go { accum: 1, power: p }
+  where
+  go :: _ -> Either _ Number
+  go { accum: acc, power: 0 } = Right acc
+  go { accum: acc, power: p } = Left { accum: acc * n, power: p - 1 }
+```
+
     tailRec :: forall a b. (a -> Either a b) -> a -> b
 
 #### `tailRecM2`
 
+Create a tail-recursive function of two arguments which uses constant stack space.
+
     tailRecM2 :: forall m a b c. (MonadRec m) => (a -> b -> m (Either { b :: b, a :: a } c)) -> a -> b -> m c
 
 #### `tailRecM3`
+
+Create a tail-recursive function of three arguments which uses constant stack space.
 
     tailRecM3 :: forall m a b c d. (MonadRec m) => (a -> b -> c -> m (Either { c :: c, b :: b, a :: a } d)) -> a -> b -> c -> m d
 
--- src/Control/Monad/Rec/Class.purs
+++ src/Control/Monad/Rec/Class.purs
@@ -20,15 +20,49 @@ import Control.Monad.Error.Trans
 import Control.Monad.State.Trans
 import Control.Monad.Writer.Trans
 
+-- | This type class captures those monads which support tail recursion in constant stack space.
+-- |
+-- | The `tailRecM` function takes a step function, and applies that step function recursively
+-- | until a return value of type `b` is found.
+-- |
+-- | Instances are provided for standard monad transformers.
+-- |
+-- | For example:
+-- |
+-- | ```purescript
+-- | loopWriter :: Number -> WriterT Sum (Eff (trace :: Trace)) Unit
+-- | loopWriter n = tailRecM go n
+-- |   where
+-- |   go 0 = do
+-- |     lift $ trace "Done!"
+-- |     return (Right unit)
+-- |   go n = do
+-- |     tell $ Sum n  
+-- |     return (Left (n - 1))
+-- | ```
 class (Monad m) <= MonadRec m where
   tailRecM :: forall a b. (a -> m (Either a b)) -> a -> m b
 
+-- | Create a tail-recursive function of two arguments which uses constant stack space.
 tailRecM2 :: forall m a b c. (MonadRec m) => (a -> b -> m (Either { a :: a, b :: b } c)) -> a -> b -> m c
 tailRecM2 f a b = tailRecM (\o -> f o.a o.b) { a: a, b: b }
 
+-- | Create a tail-recursive function of three arguments which uses constant stack space.
 tailRecM3 :: forall m a b c d. (MonadRec m) => (a -> b -> c -> m (Either { a :: a, b :: b, c :: c } d)) -> a -> b -> c -> m d
 tailRecM3 f a b c = tailRecM (\o -> f o.a o.b o.c) { a: a, b: b, c: c }
 
+-- | Create a pure tail-recursive function of one argument
+-- |
+-- | For example:
+-- |
+-- | ```purescript
+-- | pow :: Number -> Number -> Number
+-- | pow n p = tailRec go { accum: 1, power: p }
+-- |   where
+-- |   go :: _ -> Either _ Number
+-- |   go { accum: acc, power: 0 } = Right acc
+-- |   go { accum: acc, power: p } = Left { accum: acc * n, power: p - 1 }
+-- | ```
 tailRec :: forall a b. (a -> Either a b) -> a -> b
 tailRec f a = go (f a)
   where

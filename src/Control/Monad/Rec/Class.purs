42b5a06395dfed0d90bde9065575f005f789d475
Jorge Acereda <jacereda@gmail.com>
2015-02-18 23:05:17 +0100

    TailRec ADT with Loop/Done constructors to make intent more explicit

--- src/Control/Monad/Rec/Class.purs
+++ src/Control/Monad/Rec/Class.purs
@@ -1,5 +1,6 @@
 module Control.Monad.Rec.Class 
   ( MonadRec
+  , TailRec(..)
   , tailRec
   , tailRecM
   , tailRecM2
@@ -20,6 +21,16 @@ import Control.Monad.Error.Trans
 import Control.Monad.State.Trans
 import Control.Monad.Writer.Trans
 
+
+data TailRec a b = Loop a | Done b
+
+isLoop :: forall a b. TailRec a b -> Boolean
+isLoop tr = case tr of
+  Loop a -> true
+  otherwise -> false
+
+
+
 -- | This type class captures those monads which support tail recursion in constant stack space.
 -- |
 -- | The `tailRecM` function takes a step function, and applies that step function recursively
@@ -35,20 +46,20 @@ import Control.Monad.Writer.Trans
 -- |   where
 -- |   go 0 = do
 -- |     lift $ trace "Done!"
--- |     return (Right unit)
+-- |     return (Done unit)
 -- |   go n = do
 -- |     tell $ Sum n  
--- |     return (Left (n - 1))
+-- |     return (Loop (n - 1))
 -- | ```
 class (Monad m) <= MonadRec m where
-  tailRecM :: forall a b. (a -> m (Either a b)) -> a -> m b
+  tailRecM :: forall a b. (a -> m (TailRec a b)) -> a -> m b
 
 -- | Create a tail-recursive function of two arguments which uses constant stack space.
-tailRecM2 :: forall m a b c. (MonadRec m) => (a -> b -> m (Either { a :: a, b :: b } c)) -> a -> b -> m c
+tailRecM2 :: forall m a b c. (MonadRec m) => (a -> b -> m (TailRec { a :: a, b :: b } c)) -> a -> b -> m c
 tailRecM2 f a b = tailRecM (\o -> f o.a o.b) { a: a, b: b }
 
 -- | Create a tail-recursive function of three arguments which uses constant stack space.
-tailRecM3 :: forall m a b c d. (MonadRec m) => (a -> b -> c -> m (Either { a :: a, b :: b, c :: c } d)) -> a -> b -> c -> m d
+tailRecM3 :: forall m a b c d. (MonadRec m) => (a -> b -> c -> m (TailRec { a :: a, b :: b, c :: c } d)) -> a -> b -> c -> m d
 tailRecM3 f a b c = tailRecM (\o -> f o.a o.b o.c) { a: a, b: b, c: c }
 
 -- | Create a pure tail-recursive function of one argument
@@ -59,67 +70,67 @@ tailRecM3 f a b c = tailRecM (\o -> f o.a o.b o.c) { a: a, b: b, c: c }
 -- | pow :: Number -> Number -> Number
 -- | pow n p = tailRec go { accum: 1, power: p }
 -- |   where
--- |   go :: _ -> Either _ Number
--- |   go { accum: acc, power: 0 } = Right acc
--- |   go { accum: acc, power: p } = Left { accum: acc * n, power: p - 1 }
+-- |   go :: _ -> TailRec _ Number
+-- |   go { accum: acc, power: 0 } = Done acc
+-- |   go { accum: acc, power: p } = Loop { accum: acc * n, power: p - 1 }
 -- | ```
-tailRec :: forall a b. (a -> Either a b) -> a -> b
+tailRec :: forall a b. (a -> TailRec a b) -> a -> b
 tailRec f a = go (f a)
   where
-  go (Left a) = go (f a)
-  go (Right b) = b
+  go (Loop a) = go (f a)
+  go (Done b) = b
 
 instance monadRecIdentity :: MonadRec Identity where
   tailRecM f = Identity <<< tailRec (runIdentity <<< f)
 
 foreign import tailRecEff
-  "function tailRecEff(f, br, left, right, a) {\
+  "function tailRecEff(f, br, loop, done, a) {\
   \  return function() {\
   \    var r = f(a)();\
   \    while (true) {\
   \      if (br(r)) {\
-  \        var a1 = left(r);\
+  \        var a1 = loop(r);\
   \        r = f(a1)();\
   \      } else {\
-  \        return right(r);\
+  \        return done(r);\
   \      }\
   \    }\
   \  };\
   \}" :: forall a b r e. Fn5 (a -> Eff e r) (r -> Boolean) (r -> a) (r -> b) a (Eff e b)
 
 instance monadRecEff :: MonadRec (Eff eff) where
-  tailRecM f a = runFn5 tailRecEff f isLeft fromLeft fromRight a
+  tailRecM f a = runFn5 tailRecEff f isLoop fromLoop fromDone a
     where
-    fromLeft (Left a) = a
-    fromRight (Right b) = b
+    fromLoop (Loop a) = a
+    fromDone (Done b) = b
 
 instance monadRecMaybeT :: (MonadRec m) => MonadRec (MaybeT m) where
   tailRecM f = MaybeT <<< tailRecM \a -> do
     m <- runMaybeT (f a)
     return case m of
-      Nothing -> Right Nothing
-      Just (Left a) -> Left a
-      Just (Right b) -> Right (Just b) 
+      Nothing -> Done Nothing
+      Just (Loop a) -> Loop a
+      Just (Done b) -> Done (Just b) 
 
 instance monadRecErrorT :: (Error e, MonadRec m) => MonadRec (ErrorT e m) where
   tailRecM f = ErrorT <<< tailRecM \a -> do
     m <- runErrorT (f a)
     return case m of
-      Left e -> Right (Left e)
-      Right (Left a) -> Left a
-      Right (Right b) -> Right (Right b) 
+      Left e -> Done (Left e)
+      Right (Loop a) -> Loop a
+      Right (Done b) -> Done (Right b) 
 
 instance monadRecWriterT :: (Monoid w, MonadRec m) => MonadRec (WriterT w m) where
   tailRecM f a = WriterT $ tailRecM (\(Tuple a w) -> do
     Tuple m w1 <- runWriterT (f a)
     return case m of
-      Left a -> Left (Tuple a (w <> w1))
-      Right b -> Right (Tuple b (w <> w1))) (Tuple a mempty)
+      Loop a -> Loop (Tuple a (w <> w1))
+      Done b -> Done (Tuple b (w <> w1))) (Tuple a mempty)
 
 instance monadRecStateT :: (MonadRec m) => MonadRec (StateT s m) where
   tailRecM f a = StateT \s -> (tailRecM \(Tuple a s) -> do
     Tuple m s1 <- runStateT (f a) s
     return case m of
-      Left a -> Left (Tuple a s1)
-      Right b -> Right (Tuple b s1)) (Tuple a s)
+      Loop a -> Loop (Tuple a s1)
+      Done b -> Done (Tuple b s1)) (Tuple a s)
       

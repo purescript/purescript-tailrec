7d762f8386f9d8c803e15a1094ba9b981b8dd3f6
Gary Burgess <gary.burgess@gmail.com>
2016-10-05 23:39:48 +0100

    Introduce `Step` type instead of using `Either`

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -2,40 +2,38 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff())
-import Control.Monad.Eff.Console (CONSOLE(), log, logShow)
-import Control.Monad.Rec.Class (tailRec, tailRecM, tailRecM2)
-
-import Data.Either (Either(..))
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Control.Monad.Rec.Class (Step(..), tailRec, tailRecM, tailRecM2)
 
 -- | Compute the nth triangle number
 triangle :: Int -> Eff (console :: CONSOLE) Int
 triangle = tailRecM2 f 0
   where
-  f acc 0 = pure (Right acc)
+  f acc 0 = pure (Done acc)
   f acc n = do
     log $ "Accumulator: " <> show acc
-    pure (Left { a: acc + n, b: n - 1 })
+    pure (Loop { a: acc + n, b: n - 1 })
 
 loop :: Int -> Eff (console :: CONSOLE) Unit
 loop n = tailRecM go n
   where
   go 0 = do
     log "Done!"
-    pure (Right unit)
-  go n = pure (Left (n - 1))
+    pure (Done unit)
+  go n = pure (Loop (n - 1))
 
 mutual :: Int -> Boolean
-mutual = tailRec go <<< Left
+mutual = tailRec go <<< Loop
   where
-  go (Left n) = even n
-  go (Right n) = odd n
+  go (Loop n) = even n
+  go (Done n) = odd n
 
-  even 0 = Right true
-  even n = Left (Right (n - 1))
+  even 0 = Done true
+  even n = Loop (Done (n - 1))
 
-  odd 0 = Right false
-  odd n = Left (Left (n - 1))
+  odd 0 = Done false
+  odd n = Loop (Loop (n - 1))
 
 main :: Eff (console :: CONSOLE) Unit
 main = do
